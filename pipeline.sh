930c346931bdc8163fbf3797bee6f38f03a76312
ravila4 <ravila@protonmail.com>
2018-09-04 15:44:20 -0600

    Fix typo.

--- pipeline.sh
+++ pipeline.sh
@@ -2,12 +2,12 @@
 python src/data/fetch_chembl.py GPCRs 4
 python src/data/fetch_chembl.py ion-channels 4
 python src/data/fetch_chembl.py kinases 4
-python src/data/fetch_chembl.py receptors 4
+python src/data/fetch_chembl.py nuclear-receptors 4
 
 ./src/data/get_sequences.sh GPCRs
 ./src/data/get_sequences.sh ion-channels
 ./src/data/get_sequences.sh kinases
-./src/data/get_sequences.sh receptors
+./src/data/get_sequences.sh nuclear-receptors
 
 # Generate features
 python ./src/features/get_unique_ligs.py
